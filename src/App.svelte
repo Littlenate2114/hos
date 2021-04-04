4bf6261505ce6ed5e55116390509ade0ad6dd88b
Adsnipers <38774365+Adsnipers@users.noreply.github.com>
2021-04-04 13:45:13 +1000

    Merge Dev Changes (#3)
    * Transfer to Svelte
    
    * Import HOS scripts

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,30 @@
-<script>
-	export let name;
+<html lang="en" dir="ltr">
+
+<head>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,900" rel="stylesheet">
+</head>
+
+<body id="body">
+	<div class="time">
+		<h1 id="time">HH:MMAP</h1>
+		<h1 id="msg">Welcome To HOS</h1>
+	</div>
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function () {
+		setInterval(function () {
+			var d = new Date();
+			var time = d.toLocaleString('en-US', {
+				hour: 'numeric',
+				minute: 'numeric',
+				hour12: true
+			})
+			$("#time").text(time);
+		}, 1000);
+	});
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Svelte is up and running and we're ready to get creating!</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+</html>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'sam'
+		welcomeMessage: 'Welcome To HOS'
 	}
 });
 
